2d47098218203d0006143203184ad16f1387ec05
Kieroxide <kieranbprivate@gmail.com>
2025-08-12 21:26:50 +0100

    Succesfully stores the data into a db but takes way to long to get all email infomation

---
+++ py/main.py
@@ -0,0 +1,45 @@
+from auth import authenticate_gmail
+from load import get_msg_ids
+from utils import chunk_list
+from extract import get_body, get_headers
+from db import load_db
+import pandas as pd
+import sqlite3
+
+def main():
+    CHUNK_SIZE = 100
+    load_db()
+    service = authenticate_gmail()
+    ids = get_msg_ids(service)
+    
+    df = pd.DataFrame(columns=["id", "from", "to", "subject", "body", "snippet", "date"])
+    conn = sqlite3.connect('emails.db')
+
+    chunked_ids = chunk_list(ids, CHUNK_SIZE)
+    # Get a chunk of email data and store in dataframe
+    chunks_completed = 0
+    for id_chunk in chunked_ids:
+        for id in id_chunk:
+            # Extracts all required data to dataframe
+            ID = id["id"]
+
+            result = service.users().messages().get(userId="me", id=ID).execute()
+            payload = result["payload"]
+
+            SNIPPET = ""
+            if "snippet" in payload:
+                SNIPPET = payload["snippet"]
+
+            BODY = get_body(payload)
+            FROM, TO, SUBJECT, DATE = get_headers(payload)
+
+            df.loc[len(df)] = [ID, FROM, TO, SUBJECT, BODY, SNIPPET, DATE]
+        # Appends data to sql_server
+        df.to_sql("Emails", conn, if_exists="append")
+        df = df[0:0]
+        chunks_completed += 1
+        print(chunks_completed)
+
+
+if __name__ == '__main__':
+    main()
