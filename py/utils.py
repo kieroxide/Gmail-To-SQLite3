7f61fa0867c9dd885a48c0e689c814d2c6cb6756
Kieroxide <kieranbprivate@gmail.com>
2025-08-16 17:38:06 +0100

    Refactored chunk_list to be more safe

--- py/utils.py
+++ py/utils.py
@@ -1,3 +1,4 @@
 def chunk_list(lst, chunk_size):
     """Splits list into list of chunk_size lists"""
+    if(len(lst) == 0) : return []
     return [lst[i:i + chunk_size] for i in range(0, len(lst), chunk_size)]
